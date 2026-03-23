c19a408e0f29b443d65111255e4e000387a08530
navodya9434 <navodprint@gmail.com>
2026-03-23 17:21:04 +0530

    Setup React app structure and global styles

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import HomePage from "./pages/homePage";
+import React from 'react';
+
+
+function App() {
+  return <HomePage />;
+
+  return (
+    <div>
+      <h1>Event Club</h1>
+    </div>
+  );
+
+}
+export default App;
